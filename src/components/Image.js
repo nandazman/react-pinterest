04b8d1f99c0e29496d7c80f319a633110d8d51f2
nandazman <nandazmann@gmail.com>
2021-01-30 13:29:53 +0700

    change flex with grid

---
+++ src/components/Image.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import LazyLoad from 'react-lazyload';
+
+function Image({ downloadUrl, url, index, maxElement }) {
+  const updateContainerMargin = () => {
+    const upperElement = document.querySelectorAll('.image-container')[index - maxElement + 1];
+    const container = document.querySelectorAll('.image-container')[index];
+    
+    if (upperElement) {
+      const image = upperElement.querySelector('img');
+      if (image.complete) {
+        updateStyle()
+      } else {
+        image.addEventListener('load', updateStyle)
+      }
+
+      function updateStyle() {
+        const difference = (image.height + getMarginTop(upperElement.style.marginTop)) - container.offsetHeight;
+        container.style.marginTop = `${difference}px`;
+        container.style.height = container.querySelector('img').height + 'px';
+      }
+    }
+  }
+
+  const getMarginTop = (marginTop) => {
+    if (!marginTop) return 0;
+    return Number(marginTop.replace('px', ''))
+  }
+  return (
+    <LazyLoad offset={200} placeholder={<img style={{ "width": "252px", "height": "300px" }} src="https://trirama.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png" alt="plcaeholder" />}>
+      <img style={{ "width": "252px" }} src={downloadUrl} alt={url} onLoad={updateContainerMargin} />
+    </LazyLoad>
+  )
+}
+
+export default Image;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,45 +1,56 @@
-import { useState, useEffect } from 'react';
-import LazyLoad from 'react-lazyload';
+import { useState, useEffect, useReducer } from 'react';
+import Image from '../components/Image';
+
+const SET_PHOTOS = 'SET_PHOTOS';
+const ADD_PAGE = 'ADD_PAGE';
+const initState = {
+  photos: [],
+  page: 0
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_PHOTOS:
+      return {
+        ...state,
+        photos: [...state.photos, ...action.payload]
+      };
+    case ADD_PAGE:
+      return {
+        ...state,
+        page: state.page + 1
+      };
+    default:
+      return state;
+  }
+};
+
 function Main() {
-  const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
-  const [offsetHeight, setOffsetHeight] = useState([]);
-  const [totalImage, setTotalImage] = useState(0);
+  const baseHeightImage = 180;
+  const [store, dispatch] = useReducer(reducer, initState);
   const [maxElement, setMaxElement] = useState(Math.floor(window.outerWidth / 260));
 
   useEffect(() => {
-    const fetchImage = async () => {
-      const response = await fetch(`https://picsum.photos/v2/list?page=${page}`);
+    const fetchImage = async (page) => {
+      const response = await fetch(`https://picsum.photos/v2/list?page=${page + 1}`);
       const data = await response.json();
-      setPhotos([...photos, ...data]);
+      dispatch({
+        type: SET_PHOTOS,
+        payload: data,
+      });
     }
-    fetchImage(page);
-  }, [page]);
-
-  const calculateOffsetWidthEl = (index) => {
-    return index % maxElement * 260;
-  }
-
-  const updateHeightPosition = (index) => {
-    
-    const newOffSetHeight = [...offsetHeight];
-    const currentImage = document.querySelectorAll('.lazyload-wrapper')[index];
-
-    newOffSetHeight[index % maxElement] = (newOffSetHeight[index % maxElement] || 0) + currentImage.offsetHeight;
-    currentImage.style.transform = `${currentImage.style.transform.substr(0, currentImage.style.transform.indexOf('translateY'))} translateY(${offsetHeight[index % maxElement]}px)`;
-
-    setOffsetHeight(newOffSetHeight);
-
-  }
+    fetchImage(store.page);
+  }, [store.page, maxElement]);
 
   return (
-    <div className="main" style={{ "position": "relative", "maxWidth": `${maxElement * 252}px`, "margin": "auto"}}>
-      {photos.map((item, index) => (
-        <LazyLoad offset={500} key={index} style={{ "position": "absolute", "transform": `translateX(${calculateOffsetWidthEl(index)}px) translateY(${Math.floor(index / maxElement) * 300}px)`, "width": "252px" }} placeholder={<img style={{ "width": "300px", "height": "300px" }} key={index} src="https://trirama.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png" alt="plcaeholder" />}>
-          <img style={{ "width": "252px" }} key={index} src={item.download_url} alt={item.url} onLoad={() => updateHeightPosition(index)} />
-        </LazyLoad>
-      ))}
-      <button style={{ "position": "fixed", "bottom": "0"}} onClick={() => setPage(page + 1)}>Add</button>
+    <div className="main" style={{ "position": "relative", "maxWidth": `${maxElement * 260}px`, "margin": "auto", "display": "grid", "gridTemplateColumns": "repeat(auto-fit, 252px)", "justifyContent": "center", "gridAutoRows": `${baseHeightImage}px`, "gridColumnGap": "15px", "gridRowGap": "10px"}}>
+      {store.photos.map((_, i) => {
+        return (<div className="image-container" key={i} style={{ "width": "252px", "display": "flex", "flexDirection": "column"}}>
+          <Image key={i} index={i} maxElement={maxElement} downloadUrl={store.photos[i].download_url} url={store.photos[i].url} />
+        </div>)
+      })}
+      <button style={{ "position": "fixed", "bottom": "0"}} onClick={() => dispatch({
+        type: ADD_PAGE,
+      })}>Add</button>
     </div>
   )
 }
